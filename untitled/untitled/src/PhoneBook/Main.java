5265b032aa520120c2e9a3c79d810068595092ca
NikitaProkoshin1 <115005189+NikitaProkoshin1@users.noreply.github.com>
2023-01-22 22:08:48 +0700

    Add files via upload

---
+++ untitled/untitled/src/PhoneBook/Main.java
@@ -0,0 +1,11 @@
+package PhoneBook;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        PhoneBook phoneBook = new PhoneBook();
+        Controller.start(phoneBook);
+    }
+}
